7c00963a6a02a7404324708718351185b538b638
clod81 <contin@gmail.com>
2016-07-14 16:07:13 +1200

    removed

--- message_socket/statics.js
+++
@@ -1,8 +0,0 @@
-var redis = require('redis');
-
-module.exports.list = {
-  jwtSecret:   'fioasddsadasaaaaaaaaaaaxxxsdasdas312ed3$CGF%@$CF#rf431rc`2rdx$~#12132132@$$R#$@EDSASAAWD@!23d23d23d32d4457897b6v5c46v7b8v653vg%^$T',
-  io:  null, // set by the app on init
-  sio: null, // set by the app on init
-  redisClient: redis.createClient() // used to read and write into redis, not the subscriber
-};
